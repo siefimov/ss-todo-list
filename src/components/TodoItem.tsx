cc433f090692ba384d43c222ca49e7c6d5a61eaf
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-29 17:05:57 +0200

    added CRUD logic to TodoItem component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,22 +1,107 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { MdDelete, MdEdit } from 'react-icons/md';
+import { useAppDispatch } from '../store/hooks';
+import { deleteTodo, updateTodo } from '../store/todos/todosThunks';
 
 export interface ITodoItemProps {
-    id?: string;
+    id: number | string;
     title: string;
     completed?: boolean;
 }
 
-const TodoItem: FC<ITodoItemProps> = ({ title, completed }) => {
+const TodoItem: FC<ITodoItemProps> = ({ id, title, completed }) => {
+    const dispatch = useAppDispatch();
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [newTitle, setNewTitle] = useState(title);
+    const [isCompleted, setIsCompleted] = useState(completed);
+
+    const inputRef: React.RefObject<HTMLInputElement> = useRef(null);
+
+    const handleClickCheckbox = () => {
+        setIsCompleted(!isCompleted);
+        console.log(completed);
+        dispatch(updateTodo({ id: id, todo: { title: title, completed: isCompleted } }));
+    };
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleSaveClick = () => {
+        dispatch(updateTodo({ id: id, todo: { title: newTitle } }));
+        setIsEditing(false);
+    };
+
+    const handleCancelClick = () => {
+        setIsEditing(false);
+        setNewTitle(title);
+    };
+
+    useEffect(() => {
+        if (isEditing) {
+            inputRef.current?.focus();
+        }
+    }, [isEditing]);
+
     return (
-        <li className='flex justify-between items-center border rounded p-2 bg-white'>
-            <div>
-                <input type='checkbox' checked={completed} />
-                <span className='mx-4'>{title}</span>
+        <li className='flex justify-between items-center min-h-[44px] border rounded p-2 bg-white'>
+            <div className='flex items-center'>
+                {isEditing ? (
+                    <>
+                        <input type='checkbox' checked={isCompleted} />
+                        <input
+                            type='text'
+                            value={newTitle}
+                            onChange={(e) => setNewTitle(e.target.value)}
+                            onKeyUp={(e) => {
+                                if (e.key === 'Enter') {
+                                    handleSaveClick();
+                                }
+                            }}
+                            ref={inputRef}
+                            className='mx-4 outline-none text-sky-800'
+                        />
+                    </>
+                ) : (
+                    <>
+                        <input type='checkbox' checked={isCompleted} onChange={() => handleClickCheckbox()} />
+                        <span className={`mx-4 ${isCompleted ? 'text-slate-400 line-through' : ''}`}>{title}</span>
+                    </>
+                )}
             </div>
             <div className='flex'>
-                <MdEdit />
-                <MdDelete />
+                {isEditing ? (
+                    <>
+                        <div
+                            onClick={handleSaveClick}
+                            className='p-1 bg-green-100 mr-2 rounded hover:cursor-pointer hover:bg-green-200 text-xs'
+                        >
+                            Save
+                        </div>
+                        <div
+                            onClick={handleCancelClick}
+                            className='p-1 bg-gray-100 rounded hover:cursor-pointer hover:bg-gray-200 text-xs'
+                        >
+                            Cancel
+                        </div>
+                    </>
+                ) : (
+                    <div
+                        onClick={handleEditClick}
+                        className='p-1 border border-sky-400 mr-2 rounded hover:cursor-pointer hover:bg-sky-200 text-[#585858] text-base'
+                    >
+                        <MdEdit />
+                    </div>
+                )}
+                {isEditing ? null : (
+                    <div
+                        onClick={() => dispatch(deleteTodo(id))}
+                        className='p-1 border border-red-400 rounded hover:cursor-pointer hover:bg-red-200 text-slate-600'
+                    >
+                        <MdDelete />
+                    </div>
+                )}
             </div>
         </li>
     );
