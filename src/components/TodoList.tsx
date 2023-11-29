5d0986469943bf665133451a3dd3c3ca9f37484d
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-29 17:07:12 +0200

    replace getAllTodos func from TodoList to App

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,15 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 
 import TodoItem from './TodoItem';
-import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { getAllTodos } from '../store/todos/todosThunks';
+import { useAppSelector } from '../store/hooks';
 
 const TodoList: FC = () => {
     const todos = useAppSelector((state) => state.todos.list);
-    const dispatch = useAppDispatch();
-
-    useEffect(() => {
-        dispatch(getAllTodos());
-    }, [dispatch]);
 
     return (
-        <ul className='flex flex-col gap-2 max-w-[600px] bg-slate-100 p-6 rounded-md'>
+        <ul className='flex flex-col gap-3 max-w-[700px] p-6 m-4 border border-sky-300 rounded bg-slate-100'>
             {todos.map((todo) => (
-                <TodoItem key={todo.id} title={todo.title} completed={todo.completed} />
+                <TodoItem key={todo.id} title={todo.title} completed={todo.completed} id={todo.id} />
             ))}
         </ul>
     );
