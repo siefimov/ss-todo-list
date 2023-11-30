a3b5b5fce27080dbef80f16b4fad1798776cd588
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-12-01 00:40:20 +0200

    added SelectButton component
    - this component lets select All, Completed and Incomplete todos

---
+++ src/components/SelectButton.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+
+interface ISelectButtonProps {
+    value: string;
+    onChange(e: React.ChangeEvent<HTMLSelectElement>): void;
+}
+
+export const SelectButton: FC<ISelectButtonProps> = ({ value, onChange }) => (
+    <select
+        value={value}
+        onChange={onChange}
+        className='border border-slate-300 p-2 rounded text-slate-500 outline-none focus:border focus:border-slate-400 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] transition duration-200 ease-in-out'
+    >
+        <option value='all' className='appearance-none '>
+            All
+        </option>
+        <option value='incompleted' className='py-5'>
+            Incompleted
+        </option>
+        <option value='completed'>Completed</option>
+    </select>
+);
